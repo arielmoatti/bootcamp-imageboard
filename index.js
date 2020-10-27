60e2ae9e8b4a2b44216e8a77c719c7115fa0a1c5
Ariel Moatti <github@arielmoatti.com>
2020-10-27 13:41:20 +0100

    laptop sync

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
 const express = require("express");
 const app = express();
 const db = require("./db");
+/////// MULTER ////////
+// handles files and stores them in the "uploads" folder
+const multer = require("multer");
+const uidSafe = require("uid-safe");
+const path = require("path");
 
+const diskStorage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, __dirname + "/uploads");
+    },
+    filename: function (req, file, callback) {
+        uidSafe(24).then(function (uid) {
+            callback(null, uid + path.extname(file.originalname));
+        });
+    },
+});
+
+const uploader = multer({
+    storage: diskStorage,
+    limits: {
+        fileSize: 2097152,
+    },
+});
+////////////////////////////////////////////
+
+///MIDDLEWARE/////
 app.use(express.static("public"));
+//////////////////
 
+///// ROUTES ///////
 app.get("/images", (req, res) => {
     db.getImages()
         .then(({ rows }) => {
@@ -14,5 +41,19 @@ app.get("/images", (req, res) => {
         });
 });
 
+app.post("/upload", uploader.single("file"), (req, res) => {
+    // console.log("input values: ", req.body);
+    // console.log("file: ", req.file);
+
+    if (req.file) {
+        res.json({
+            success: true,
+        });
+    } else {
+        res.json({
+            success: false,
+        });
+    }
+});
 //
 app.listen(8080, () => console.log("image board up and running"));
--- public/js/script.js
+++ public/js/script.js
@@ -5,15 +5,49 @@ new Vue({
     el: "#main",
     //this is our data object, which vue will render
     data: {
-        name: "Ariel",
-        seen: false,
         images: [], //we leave it an empty array
+        //data properties that will store values to Vue logic
+        title: "", //placeholder
+        description: "",
+        username: "",
+        file: null,
     },
-    methods: {},
+    // all created functions are defined here
+    methods: {
+        handleClick: function (e) {
+            e.preventDefault();
+            // console.log("this: ", this);
+
+            let formData = new FormData(); //we need this to send a *file*
+            formData.append("title", this.title);
+            formData.append("description", this.description);
+            formData.append("username", this.username);
+            formData.append("file", this.file);
+            //
+            axios
+                .post("/upload", formData)
+                .then(function (response) {
+                    console.log("response from POST /upload", response);
+                })
+                .catch(function (err) {
+                    console.log("error in axios POST /upload", err);
+                });
+        },
+        handleChange: function (e) {
+            // console.log("handleChange is running!");
+            // console.log("file: ", e.target.files[0]);
+            this.file = e.target.files[0]; //grabbing the file from the choose file button
+        },
+    },
     mounted: function () {
         var me = this; //helps to pass the global "this"  (main data in vue) down to the function below
-        axios.get("/images").then(function (response) {
-            me.images = response.data;
-        });
+        axios
+            .get("/images")
+            .then(function (response) {
+                me.images = response.data;
+            })
+            .catch(function (err) {
+                console.log("error in axios GET /images", err);
+            });
     },
 });
